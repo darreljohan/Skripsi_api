683ed59c25c76a941ede78c17688eea5933c8f21
listenerservice <darreljohan162@gmail.com>
2024-08-21 13:08:14 +0700

    -- micro high load

--- k6_mono_api/src/microservice/file-service.js
+++ k6_mono_api/src/microservice/file-service.js
@@ -5,35 +5,45 @@ import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 const mockFile = open("./mock-file.jpg", "b");
 
-const API_URL = "http://47.129.192.138:80/api/file";
+const API_URL = "http://172.31.34.226:80/api/file";
 const HEADER = {
   headers: {
-    "X-API-TOKEN": "b63d314c-ad54-4059-8e78-680e0ac5bd58",
+    "X-API-TOKEN": "da8a4d23-1559-4afc-aa1c-e08da5fb1a75",
   },
 };
 const DATA = {
-  id: 6,
+  id: 2,
   file: http.file(mockFile, "mocking.jpg"),
 };
 
 export const options = {
   cloud: {
     // Project: Load Testing
-    projectID: 3709926,
+    projectID: 3710305,
     // Test runs with the same name groups test runs together.
-    name: "Micro File",
+    name: "Micro file",
   },
   stages: [
+    { duration: "2ms", target: 10 },
+    { duration: "300s", target: 10 },
     { duration: "2ms", target: 20 },
-    { duration: "120s", target: 20 },
+    { duration: "300s", target: 20 },
+    { duration: "2ms", target: 30 },
+    { duration: "300s", target: 30 },
     { duration: "2ms", target: 40 },
-    { duration: "120s", target: 40 },
+    { duration: "300s", target: 40 },
+    { duration: "2ms", target: 50 },
+    { duration: "300s", target: 50 },
     { duration: "2ms", target: 60 },
-    { duration: "120s", target: 60 },
+    { duration: "300s", target: 60 },
+    { duration: "2ms", target: 70 },
+    { duration: "300s", target: 70 },
     { duration: "2ms", target: 80 },
-    { duration: "120s", target: 80 },
+    { duration: "300s", target: 80 },
+    { duration: "2ms", target: 90 },
+    { duration: "300s", target: 90 },
     { duration: "2ms", target: 100 },
-    { duration: "120s", target: 100 },
+    { duration: "300s", target: 100 },
   ],
   // Output results to CSV
 };
@@ -46,5 +56,5 @@ export default function () {
   check(response, {
     "is status 200": (r) => r.status === 200,
   });
-  sleep(0.5);
+  sleep(1);
 }
--- k6_mono_api/src/microservice/user-service.js
+++ k6_mono_api/src/microservice/user-service.js
@@ -3,31 +3,41 @@ import { check, sleep } from "k6";
 import { Trend } from "k6/metrics";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
-const API_URL = "http://47.129.192.138:80/api/users/current";
+const API_URL = "http://172.31.34.226:80/api/users/current";
 const HEADER = {
   headers: {
-    "X-API-TOKEN": "b63d314c-ad54-4059-8e78-680e0ac5bd58",
+    "X-API-TOKEN": "da8a4d23-1559-4afc-aa1c-e08da5fb1a75",
   },
 };
 
 export const options = {
   cloud: {
     // Project: Load Testing
-    projectID: 3709926,
+    projectID: 3710305,
     // Test runs with the same name groups test runs together.
-    name: "Micro User",
+    name: "Micro user",
   },
   stages: [
+    { duration: "2ms", target: 10 },
+    { duration: "300s", target: 10 },
     { duration: "2ms", target: 20 },
-    { duration: "120s", target: 20 },
+    { duration: "300s", target: 20 },
+    { duration: "2ms", target: 30 },
+    { duration: "300s", target: 30 },
     { duration: "2ms", target: 40 },
-    { duration: "120s", target: 40 },
+    { duration: "300s", target: 40 },
+    { duration: "2ms", target: 50 },
+    { duration: "300s", target: 50 },
     { duration: "2ms", target: 60 },
-    { duration: "120s", target: 60 },
+    { duration: "300s", target: 60 },
+    { duration: "2ms", target: 70 },
+    { duration: "300s", target: 70 },
     { duration: "2ms", target: 80 },
-    { duration: "120s", target: 80 },
+    { duration: "300s", target: 80 },
+    { duration: "2ms", target: 90 },
+    { duration: "300s", target: 90 },
     { duration: "2ms", target: 100 },
-    { duration: "120s", target: 100 },
+    { duration: "300s", target: 100 },
   ],
   // Output results to CSV
 };
@@ -40,5 +50,5 @@ export default function () {
   check(response, {
     "is status 200": (r) => r.status === 200,
   });
-  sleep(0.5);
+  sleep(1);
 }
